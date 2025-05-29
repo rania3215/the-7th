bb75f3e48611397defd96cdf4bfa120e31a8d030
Rania Atmeh <142887521+rania3215@users.noreply.github.com>
2025-05-29 12:37:46 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -68,16 +68,13 @@ emotion_comments = {
 
 def preprocess_image(img):
     transform = transforms.Compose([
-   transforms.RandomResizedCrop(224),  # Random crop and resize to 224x224
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(20),
         transforms.Resize((224, 224)),
-    transforms.ColorJitter(brightness=0.2, contrast=0.2),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) 
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
     ])
 
-   
+
 
 
 
