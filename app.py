78e656155a44946d0562ba46231539e99149b964
Rania Atmeh <142887521+rania3215@users.noreply.github.com>
2025-06-11 17:15:03 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -37,9 +37,9 @@ class EnsembleNet(nn.Module):
         return self.classifier(x)
 
 # -------------------------------
-MODEL_ID = "1-x445n-cKYNiWLPWsYaLnHKCyX465NAj"
+MODEL_ID = "1duVtQtRKpvzPGd1TYr8BgNyblz_2QNno"
 MODEL_URL = f"https://drive.google.com/uc?id={MODEL_ID}"
-MODEL_PATH = "7_emotions_model.pth"
+MODEL_PATH = "7th_emotions_model.pth"
 
 if not os.path.exists(MODEL_PATH):
     with st.spinner("Downloading model..."):
@@ -70,8 +70,8 @@ def preprocess_image(img):
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                             std=[0.5, 0.5, 0.5])
     ])
 
 
